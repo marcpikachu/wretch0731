673330511c6b760cd1275b1df9f91c5328b82ff9
marcpikachu <forcervp0206@gmail.com>
2025-08-04 15:42:31 +0800

    整理檔案路徑

--- wretch/urls.py
+++ wretch/urls.py
@@ -14,19 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
+from django.contrib import admin
 from django.urls import path
 # from django.http import HttpResponse
-from django.shortcuts import render
+from .views import about, home
 
-def home(request):
-    return render(request, "home.html")
-
-def about(request):
-    return render(request, "about.html")
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('about/', about),
     path("", home),
 ]
