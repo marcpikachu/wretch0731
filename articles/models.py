1e30ca9e6df03a24e763a88955a16eed4882cd81
marcpikachu <forcervp0206@gmail.com>
2025-08-07 11:28:03 +0800

    add  __str__(self)魔術

--- articles/models.py
+++ articles/models.py
@@ -5,8 +5,5 @@ class Article(models.Model):
     title = models.CharField(max_length=200, null=False)
     content = models.TextField(null=True)
 
-# 1. 編輯 models.py
-# 2. 執行 uv run python manage.py makemigrations APP_NAME
-# 3. 檢查 uv run python manage.py sqlmigrate APP_NAME 編號
-# 4. 執行 uv run python manage.py migrate APP_NAME
-# 5. 看資料表是否產生
+    def __str__(self):
+        return self.title
