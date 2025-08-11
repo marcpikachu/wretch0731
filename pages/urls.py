a73a203df741d43cd554025300c85534a0c24d9a
marcpikachu <forcervp0206@gmail.com>
2025-08-11 11:39:45 +0800

    feat:htmx套件

--- pages/urls.py
+++ pages/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
     path("", index, name="home"),
+    path("test/", views.test, name="test"),
+    
 ]
--- pages/views.py
+++ pages/views.py
@@ -9,3 +9,6 @@ def about(request):
 
 def contact(request):
     return render(request, "pages/contact.html")
+
+def test(request):
+    return render(request, "pages/test.html")
